44f2fc6d0a53fcee53a51aac214752b2b6a0f85c
Aayush <aayushk4980@gmail.com>
2024-11-02 00:16:02 +0530

    Update AuthRoute.js

--- routes/AuthRoute.js
+++ routes/AuthRoute.js
@@ -31,6 +31,15 @@ router.get("/user", authMiddleware, async (req, res) => {
 router.post("/signup", async (req, res) => {
     try {
         const hashedPassword = await bycrypt.hash(req.body.password, 10);
+
+        const email = req.body.email;
+
+        const existingUser = await User.findOne({ email });
+
+        if (existingUser) {
+            return res.status(400).json({ message: "Email already in use" });
+        }
+
         const user = new User({
             name: req.body.name,
             email: req.body.email,
@@ -41,7 +50,7 @@ router.post("/signup", async (req, res) => {
 
         res.status(201).json({ message: "User registered successfully" });
     } catch (error) {
-        res.status(400).json({ message: "Error registering user" });
+        res.status(400).json({ message: "Error registering user", error });
     }
 });
 
