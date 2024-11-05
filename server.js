1cdfb56acaa27141691772d88776d3d5629fccba
Aayush <aayushk4980@gmail.com>
2024-11-05 19:43:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import cors from "cors";
 import ProductRoute from "./routes/ProductRoute.js";
 import AddProductRoute from "./routes/AddProductRoute.js";
 import AuthRoute from "./routes/AuthRoute.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -12,27 +13,30 @@ const app = express();
 
 app.use(express.json());
 
+app.use(cookieParser());
+
 app.use(cors({
-    origin: "*",
+    origin: "http://localhost:5173", // Frontend URL
+    credentials: true,               // Allows cookies to be sent
 }));
 
 const port = process.env.PORT || 3000;
 
 const connectToMongoDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGODB_URI);
-        console.log("Connected to MongoDB");
-    } catch (error) {
-        console.log(error);
-    }
+	try {
+		await mongoose.connect(process.env.MONGODB_URI);
+		console.log("Connected to MongoDB");
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 connectToMongoDB();
 
 app.get("/", (req, res) => {
-  res.json({
-    "/products": "Get all products",
-  });
+	res.json({
+		"/products": "Get all products",
+	});
 });
 
 app.use("/products", ProductRoute);
@@ -40,5 +44,5 @@ app.use("/addProduct", AddProductRoute);
 app.use("/auth", AuthRoute);
 
 app.listen(port, () => {
-  console.log("Server is running on port", port);
+	console.log("Server is running on port", port);
 });
